b7d74ad442da785b2bd174e69ced8e4ee468b177
Matthias Seghers <19349429+matthiasseghers@users.noreply.github.com>
2022-12-16 13:34:05 +0100

    Fix dark mode on launch script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,10 @@
+enableDarkMode(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function (e) {
-    document.body.classList.add(e.matches ? "dark" : "light");
-    document.body.classList.remove(e.matches ? "light" : "dark");
+    enableDarkMode(e.matches);
 });
+
+const enableDarkMode = (enable = true) => {
+    document.body.classList.add(enable ? "dark" : "light");
+    document.body.classList.remove(enable ? "light" : "dark");
+}
