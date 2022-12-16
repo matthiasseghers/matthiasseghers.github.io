64b16d9c64d41d35647bde1e5f4ced6f1fc9dac7
Matthias Seghers <19349429+matthiasseghers@users.noreply.github.com>
2022-12-16 13:41:17 +0100

    Fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
+const enableDarkMode = (enable = true) => {
+    document.body.classList.add(enable ? "dark" : "light");
+    document.body.classList.remove(enable ? "light" : "dark");
+}
+
 enableDarkMode(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function (e) {
     enableDarkMode(e.matches);
 });
-
-const enableDarkMode = (enable = true) => {
-    document.body.classList.add(enable ? "dark" : "light");
-    document.body.classList.remove(enable ? "light" : "dark");
-}
